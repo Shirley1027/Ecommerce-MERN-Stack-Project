d456768d0882b46deedabd622c701ca424c1df96
Shirley Huang <shirleyhuang1027@gmail.com>
2023-04-01 21:48:55 +0800

    add user update profile

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import { requireSignin, isAdmin } from "../middwares/auth.js";
 
 //controllers
-import { register, login, secret } from "../controllers/auth.js";
+import { register, login, secret, updateProfile } from "../controllers/auth.js";
 router.post("/register", register);
 router.post("/login", login);
 router.get("/auth-check", requireSignin, (req, res) => {
@@ -14,7 +14,7 @@ router.get("/auth-check", requireSignin, (req, res) => {
 router.get("/admin-check", requireSignin, isAdmin, (req, res) => {
   res.json({ ok: true });
 });
-
+router.put("/profile", requireSignin, updateProfile)
 //testing
 router.get("/secret", requireSignin, isAdmin, secret);
 
