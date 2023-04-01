0c25674322ca49776f8e1aa9ef4a6b07ce4a5594
Shirley Huang <shirleyhuang1027@gmail.com>
2023-04-02 03:22:03 +0800

    new files

---
+++ client/src/ components/cards/UserCartSidebar.js
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../../context/auth";
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../../context/cart";
+import axios from "axios";
+import DropIn from "braintree-web-drop-in-react";
+
+export default function UserCartSidebar() {
+  const [auth, setAuth] = useAuth();
+  const [cart, setCart] = useCart();
+
+  const [clientToken, setClientToken] = useState("");
+  const [instance, setInstance] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth?.token) {
+      getClientToken();
+    }
+  }, [auth?.token]);
+
+  const getClientToken = async () => {
+    try {
+      const { data } = await axios.get("/braintree/token");
+      setClientToken(data.clientToken);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const cartTotal = () => {
+    let total = 0;
+    cart.map((item) => {
+      total += item.price;
+    });
+    return total.toLocaleString("CNY", {
+      style: "currency",
+      currency: "CNY",
+    });
+  };
+  return (
+    <div className="col-md-4">
+      <h4>Your Cart Summary</h4>
+      Total/Address/Pay
+      <hr />
+      <h5>Total: {cartTotal()}</h5>
+      {auth.user?.address ? (
+        <>
+          <div className="mb-3">
+            <hr />
+            <h4>Delivery Address:</h4>
+            <h5>{auth?.user?.address}</h5>
+          </div>
+          <button
+            className="btn btn-outline-warning"
+            onClick={() => navigate("/dashboard/user/profile")}
+          >
+            Update address
+          </button>
+        </>
+      ) : (
+        <div className="mb-3">
+          {auth?.token ? (
+            <button
+              className="btn btn-outline-warning mt-3"
+              onClick={() => navigate("/dashboard/user/profile")}
+            >
+              Add delivery address
+            </button>
+          ) : (
+            <button
+              className="btn btn-outline-danger mt-3"
+              onClick={() => navigate("/login", { state: "/cart" })}
+            >
+              Login to Checkout
+            </button>
+          )}
+        </div>
+      )}
+      <div>
+        {!clientToken || !cart?.length ? (
+          ""
+        ) : (
+          <>
+            <DropIn
+              options={{
+                authorization: clientToken,
+                paypal: {
+                  flow: "vault",
+                },
+              }}
+              // get instance and send to backend
+              onInstance={(instance) => setInstance(instance)}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
